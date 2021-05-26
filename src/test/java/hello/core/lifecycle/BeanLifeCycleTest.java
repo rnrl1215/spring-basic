da0a125c229c50ad1a2424c6ad49cf355b1a2f05
skahn1215 <d1215@naver.com>
2021-05-26 22:48:38 +0900

    added AllBeanTest Annotation

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,32 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        //ApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        // 변경함. close를 사용하기 위함.
+        // close를 사용할 이유가 많지 않다.
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
