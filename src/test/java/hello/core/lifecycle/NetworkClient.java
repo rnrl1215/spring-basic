b8d5421e8d057df9eb0deaad8751ce33b587a929
skahn1215 <d1215@naver.com>
2021-05-29 16:18:47 +0900

    added AllBeanTest Annotation

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,30 +1,52 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출 ,url = "+url);
-        connect();
-        call("초기화 연결 메시지");
+
+        // move to afterPropertiesSet
+        //connect();
+        //call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
+
         this.url = url;
     }
 
     //서비스 시작시 호출 메서드
     public void connect() {
+
         System.out.println("connect: "+url);
     }
 
     public void call(String message) {
+
         System.out.println("call: " + url + " message ");
     }
 
     // 서비스 종료시 호출
     public void disconnect() {
+
         System.out.println("close: "+ url);
+    }
+
+    // 빈생성후 초기화
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    //컨테이너가 내려가고 빈 이 죽을때 함수호출
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
